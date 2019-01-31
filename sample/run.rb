d98b6f4d59c2815ce1fd4b8295d2d752a0522cc7
Ziphil <yfe23457@nifty.com>
2019-02-01 00:54:42 +0900

    サンプルにマクロの利用例を追加

--- sample/run.rb
+++ sample/run.rb
@@ -13,6 +13,15 @@ $stdout.sync = true
 directory = File.dirname($0).encode("utf-8")
 source = File.read(directory + "/sample.zml")
 parser = ZenithalParser.new(source)
+
+parser.register_macro("plus") do |attributes, children_list|
+  children = children_list.first
+  result = attributes["a"].to_i + attributes["b"].to_i
+  result_element = Element.new("strong")
+  result_element.text = "#{result}"
+  next [children.first, result_element]
+end
+
 File.open(directory + "/sample.xml", "w") do |file|
   formatter = Formatters::Default.new
   document = parser.parse
