997bce651b8a2b8dd4f055747bb6d0f595363b8e
Ziphil <yfe23457@nifty.com>
2019-09-28 01:09:57 +0900

    新パーサーが入れ子のノードをパースできるよう処理を追加

--- source/zenml/new_parser.rb
+++ source/zenml/new_parser.rb
@@ -29,6 +29,7 @@ class ZenithalNewParser
   SLASH_START = "/"
   SLASH_END = "/"
   COMMENT_DELIMITER = "#"
+  SYSTEM_INSTRUCTION_NAME = "zml"
   MARKS = {:instruction => "?", :trim => "*", :verbal => "~", :multiple => "+"}
   ESCAPE_CHARS = ["&", "<", ">", "'", "\"", "{", "}", "[", "]", "/", "\\", "|", "`", "#"]
   SPACE_CHARS = [0x20, 0x9, 0xD, 0xA]
@@ -67,15 +68,104 @@ class ZenithalNewParser
   def parse
   end
 
+  def parse_nodes(verbal)
+    parser = Parser.exec(self) do
+      nodes = Nodes[]
+      raw_nodes = !(parse_element | parse_text(verbal)).many
+      raw_nodes.each do |raw_node|
+        nodes << raw_node
+      end
+      next nodes
+    end
+    return parser
+  end
+
   def parse_element
     parser = Parser.exec(self) do
       !parse_char(TAG_START)
       name = !parse_identifier
       marks = !parse_marks
-      attributes = !parse_attributes.maybe
-      next name, marks, attributes
+      attributes = !parse_attributes.maybe || {}
+      children_list = !parse_children_list
+      if name == SYSTEM_INSTRUCTION_NAME
+        !parse_space
+      end
+      next create_nodes(name, marks, attributes, children_list)
     end
     return parser
+  end
+
+  def create_nodes(name, marks, attributes, children_list)
+    nodes = Nodes[]
+    unless marks.include?(:macro)
+      if marks.include?(:instruction)
+        unless children_list.size <= 1
+          throw(:error, error_message("Processing instruction cannot have more than one argument"))
+        end
+        nodes = create_instructions(name, attributes, children_list.first)
+      else
+        unless marks.include?(:multiple) || children_list.size <= 1
+          throw(:error, error_message("Normal node cannot have more than one argument"))
+        end
+        nodes = create_elements(name, attributes, children_list)
+      end
+    else
+      nodes = process_macro(name, attributes, children_list)
+    end
+    return nodes
+  end
+
+  def create_instructions(target, attributes, children)
+    instructions = Nodes[]
+    if target == SYSTEM_INSTRUCTION_NAME
+      @version = attributes["version"] if attributes["version"]
+      @brace_name = attributes["brace"] if attributes["brace"]
+      @bracket_name = attributes["bracket"] if attributes["bracket"]
+      @slash_name = attributes["slash"] if attributes["slash"]
+    elsif target == "xml"
+      instruction = XMLDecl.new
+      instruction.version = attributes["version"] || XMLDecl::DEFAULT_VERSION
+      instruction.encoding = attributes["encoding"]
+      instruction.standalone = attributes["standalone"]
+      instructions << instruction
+    else
+      instruction = Instruction.new(target)
+      actual_contents = []
+      attributes.each do |key, value|
+        actual_contents << "#{key}=\"#{value}\""
+      end
+      if children.first && !children.first.empty?
+        actual_contents << children.first
+      end
+      instruction.content = actual_contents.join(" ")
+      instructions << instruction
+    end
+    return instructions
+  end
+
+  def create_elements(name, attributes, children_list)
+    elements = Nodes[]
+    children_list.each do |children|
+      element = Element.new(name)
+      attributes.each do |key, value|
+        element.add_attribute(key, value)
+      end
+      children.each do |child|
+        element.add(child)
+      end
+      elements << element
+    end
+    return elements
+  end
+
+  def process_macro(name, attributes, children_list)
+    elements = Nodes[]
+    if @macros.key?(name)
+      elements = @macros[name].call(attributes, children_list)    
+    else
+      throw(:error, error_message("No such macro"))
+    end
+    return elements
   end
 
   def parse_marks
@@ -121,16 +211,67 @@ class ZenithalNewParser
   def parse_quoted_string
     parser = Parser.exec(self) do
       !parse_char(ATTRIBUTE_VALUE_START)
-      texts = !(parse_quoted_string_text | parse_escape).many
+      texts = !(parse_quoted_string_content | parse_escape).many
       !parse_char(ATTRIBUTE_VALUE_END)
       next texts.join
     end
     return parser
   end
 
-  def parse_quoted_string_text
+  def parse_quoted_string_content
     parser = Parser.exec(self) do
       chars = !parse_char_out([ATTRIBUTE_VALUE_END, ESCAPE_START]).many(1)
+      next chars.join
+    end
+    return parser
+  end
+
+  def parse_children_list
+    parser = Parser.exec(self) do
+      first_children = !(parse_empty_children | parse_children)
+      rest_children_list = !parse_children.many
+      children_list = [first_children] + rest_children_list
+      next children_list
+    end
+    return parser
+  end
+
+  def parse_children
+    parser = Parser.exec(self) do
+      !parse_char(CONTENT_START)
+      children = !parse_nodes(false)
+      !parse_char(CONTENT_END)
+      next children
+    end
+    return parser
+  end
+
+  def parse_empty_children
+    parser = Parser.exec(self) do
+      !parse_char(CONTENT_END)
+      next Nodes[]
+    end
+    return parser
+  end
+
+  def parse_text(verbal)
+    parser = Parser.exec(self) do
+      texts = !(parse_text_plain(verbal) | parse_escape).many(1)
+      next Text.new(texts.join, true, nil, false)
+    end
+    return parser
+  end
+
+  def parse_text_plain(verbal)
+    parser = Parser.exec(self) do
+      out_chars = [ESCAPE_START, CONTENT_END]
+      unless verbal
+        out_chars.push(TAG_START, MACRO_START, CONTENT_START)
+        out_chars.push(BRACE_START) if @brace_name
+        out_chars.push(BRACKET_START) if @bracket_name
+        out_chars.push(SLASH_START) if @slash_name
+      end
+      chars = !parse_char_out(out_chars).many(1)
       next chars.join
     end
     return parser
